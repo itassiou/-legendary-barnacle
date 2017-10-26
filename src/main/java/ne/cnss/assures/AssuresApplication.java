077331f180af42b34e1faad3703198f863db7905
@itassiou <issoufoutassiou@gmail.com>
2017-10-26 15:14:07 +0100

    Commit first

---
+++ src/main/java/ne/cnss/assures/AssuresApplication.java
@@ -0,0 +1,12 @@
+package ne.cnss.assures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssuresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssuresApplication.class, args);
+	}
+}
